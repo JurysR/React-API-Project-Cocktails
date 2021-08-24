36bd0631d50d0bddb74cd3ccdfc93a42a8894b8d
JurysR <jurys.r@gmail.com>
2021-08-24 11:00:46 +0200

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id ab
+        molestiae nihil sit aliquam doloribus, expedita at modi nostrum,
+        deleniti quia eligendi vel tempore voluptates voluptate enim, illum esse
+        hic nisi quisquam ad velit nulla perferendis ratione. Tenetur, dolores
+        numquam?
+      </p>
+    </section>
+  );
+};
+
+export default About;
